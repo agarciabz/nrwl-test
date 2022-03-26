0a8c672387b7e3a9d409395902e7febf066a46ea
agarciabz <agusgb93@gmail.com>
2022-03-26 18:41:18 +0100

    Add detail display by param

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,8 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { BackendService, Ticket } from '../backend.service';
 
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
   styleUrls: ['./ticket.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TicketComponent {}
+export class TicketComponent implements OnInit {
+  public ticket$: Observable<Ticket> | undefined;
+  public ticketId: number | undefined;
+
+  constructor(private backend: BackendService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.ticketId = +this.route.snapshot.paramMap.get('id');
+    this.ticket$ = this.backend.ticket(this.ticketId);
+  }
+}
