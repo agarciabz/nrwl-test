d1a7fb1f09b07d426ee708876ae430261d171a03
agarciabz <agusgb93@gmail.com>
2022-03-27 19:06:25 +0200

    Revert store change for detail

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { Ticket } from '../backend.service';
+import { BackendService } from '../backend.service';
+
 import { getTicket } from '../reducers/ticket.actions';
 import { selectCurrentTicket } from '../reducers/ticket.selectors';
 
@@ -15,10 +15,15 @@ export class TicketComponent implements OnInit {
   public ticket$ = this.store.select(selectCurrentTicket);
   public ticketId: number | undefined;
 
-  constructor(private route: ActivatedRoute, private store: Store) {}
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store,
+    private backend: BackendService
+  ) {}
 
   ngOnInit() {
     this.ticketId = +this.route.snapshot.paramMap.get('id');
-    this.store.dispatch(getTicket({ ticketId: this.ticketId }));
+    // this.store.dispatch(getTicket({ ticketId: this.ticketId }));
+    this.ticket$ = this.backend.ticket(this.ticketId);
   }
 }
